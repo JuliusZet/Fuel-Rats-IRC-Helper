dbdb8803ae6ef9c7e544e9b11bb8892dae3fe557
Julius Zitzmann <info@julius-zitzmann.de>
2021-03-07 14:04:57 +0100

    Changed the Target Framework Version to v4.8

--- Fuel Rats IRC Helper/Properties/Resources.Designer.cs
+++ Fuel Rats IRC Helper/Properties/Resources.Designer.cs
@@ -19,16 +19,16 @@ namespace Fuel_Rats_IRC_Helper.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources
     {
-
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
         internal Resources()
         {
@@ -42,7 +42,7 @@ namespace Fuel_Rats_IRC_Helper.Properties
         {
             get
             {
-                if ((resourceMan == null))
+                if (object.ReferenceEquals(resourceMan, null))
                 {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Fuel_Rats_IRC_Helper.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
