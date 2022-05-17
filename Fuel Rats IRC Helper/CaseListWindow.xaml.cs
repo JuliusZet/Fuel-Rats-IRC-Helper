49ae022c6176d33e05e5491f8922455f6f428227
RexTheCapt <6693554+RexTheCapt@users.noreply.github.com>
2022-05-17 21:39:46 +0200

    Delete cases with middle mouse click

--- Fuel Rats IRC Helper/CaseListWindow.xaml.cs
+++ Fuel Rats IRC Helper/CaseListWindow.xaml.cs
@@ -42,5 +42,16 @@ namespace Fuel_Rats_IRC_Helper
         {
             (datagridCaseList.CurrentItem as Case).ShowCaseWindow();
         }
+
+        private void DataGridRow_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.MiddleButton == System.Windows.Input.MouseButtonState.Pressed)
+            {
+                System.Windows.Controls.DataGridRow dataGridRow = sender as System.Windows.Controls.DataGridRow;
+                Case c = dataGridRow.DataContext as Case;
+                Irc._Case.Remove(c);
+                Irc.RefreshCaseList();
+            }
+        }
     }
 }
